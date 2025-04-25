83a06dcdb2cda5f25fbc29407434646da1d6a036
SANKHA SUBHRA GHOSH <121628135+SANKHA-SUBHRA2003@users.noreply.github.com>
2025-04-25 22:10:42 +0530

    Food Symphony

---
+++ login/login.js
@@ -0,0 +1,10 @@
+document.querySelector('.owner').addEventListener('click', () => {
+    // Replace with actual Google Login or route
+    window.location.href = "../admin/admin.html";
+  });
+  
+  document.querySelector('.customer').addEventListener('click', () => {
+    // Replace with phone OTP login route
+    window.location.href = "../client/client.html";
+  });
+  
---
+++ script.js
@@ -0,0 +1,5 @@
+// Auto-redirect to the next page after 4 seconds
+setTimeout(() => {
+    window.location.href = "./login/login.html"; // replace with your actual next page
+  }, 5000); // 5 seconds
+  
